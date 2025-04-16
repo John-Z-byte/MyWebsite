d0a990c0b3f28b178c6440041ba71bd08058e9d4
John-Z-byte <estebandrums1@gmail.com>
2025-04-16 18:42:59 -0500

    Deploy: moved files to root for GitHub Pages

--- script.js
+++ script.js
@@ -1,28 +1,97 @@
-// Mobile Menu Toggle
-const toggleButton = document.getElementById('toggle');
-const menu = document.querySelector('.menu');
 
-toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('active');
+const menu = document.querySelector('.menu');
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click',function(){
+    menu.classList.toggle('active');
 });
 
-// Fade-in Animation on Scroll
-const fadeInElements = document.querySelectorAll('.fade-in');
 
-const checkVisibility = () => {
-  fadeInElements.forEach((element) => {
-    const rect = element.getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom >= 0) {
-      element.classList.add('visible');
-    } else {
-      element.classList.remove('visible');
+const Navbar = document.querySelector('.navbar')
+ 
+window.addEventListener('scroll',function(){
+    let scroll = this.window.scrollY;
+    if ( scroll > 100){
+        Navbar.classList.add('sticky');
     }
-  });
-};
+    else{
+        Navbar.classList.remove('sticky');
+    }
+})
 
-// Trigger the animation on page load and on scroll
-window.addEventListener('scroll', checkVisibility);
-window.addEventListener('load', checkVisibility);
+window.onload = function(){
+    new Splide('.MySplide',{
+        type:'loop',
+        drag:'free',
+        focus:'center',
+        perPage:2.5,
+        gap:'2rem',
+        pagination:false,
+        breakpoints:{
+            1400:{
+                perPage:2
+            },
+            1200:{
+                perPage:1.5
+            },
+            500:{
+                perPage:1
+            }
+        }
+        
+    }).mount({ AutoScroll: window.splide.Extensions.AutoScroll})
+}
 
-// Initial check when the page loads
-checkVisibility();
+
+var tstsplide = new Splide('.tst_splide',{
+    type:'loop',
+    drag:'free',
+    focus:'center',
+    perPage:3,
+    gap:'2rem',
+    pagination:false,
+    autoplay:true,
+    loop:true,
+    perMove:1,
+    breakpoints:{
+        1200:{
+            perPage:2
+        },
+        900:{
+            perPage:1.5
+        },
+        500:{
+            perPage:1
+        }
+    }
+});
+tstsplide.mount();
+
+
+
+var companies_splide = new Splide('.companies_splide',{
+    type:'loop',
+    drag:'free',
+    focus:'center',
+    perPage:5,
+    gap:'1rem',
+    pagination:false,
+    autoplay:true,
+    loop:true,
+    perMove:1,
+    breakpoints:{
+        1400:{
+            perPage:4
+        },
+        900:{
+            perPage:3
+        },
+        768:{
+            perPage:2
+        },
+        500:{
+            perPage:1.5
+        }
+    }
+});
+companies_splide.mount();
